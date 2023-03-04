8b73280b6eb0139cd2d38670972cca1ca16632b6
SpikeHD <spikegdofficial@gmail.com>
2023-03-04 14:05:43 -0800

    tnt chain reactions

--- scr/tnt.gd
+++ scr/tnt.gd
@@ -4,6 +4,53 @@ var HEALTH = 3
 var EMPTY = preload("res://tex/mined bg.png")
 var rng = RandomNumberGenerator.new()
 
+func explode():
+	var collider = self.get_child(1)
+	var coord = self.name.split("Dirt")[1].split("-")
+	
+	if collider == null:
+		return
+		
+	collider.queue_free()
+		
+	var sprite = self.get_child(0)
+		
+	if sprite != null:
+		sprite.texture = EMPTY
+		sprite.z_index = -1
+		
+	# Find destroy all blocks in a circle
+	const neighbors = [
+		[-2, -1], [-2, 0], [-2, 1],
+		[-1, -2], [-1, -1], [-1, 0], [-1, 1], [-1, 2],
+		[0, -2], [0, -1], [0, 1], [0, 2],
+		[1, -2], [1, -1], [1, 0], [1, 1], [1, 2],
+		[2, -1], [2, 0], [2, 1]
+	];
+		
+	for n in neighbors:
+		var x = int(coord[0]) + n[0]
+		var y = int(coord[1]) + n[1]
+		var block = get_node("/root/Node2D/SubViewportContainer/SubViewport/Dirt" + str(x) + "-" + str(y))
+		
+		if block.get_child_count() < 2:
+			continue
+		
+		var blk_collide = block.get_child(1)
+		
+		if blk_collide != null:
+			if block.has_method("explode") and block.HEALTH > 0:
+				block.explode()
+				continue
+			
+			blk_collide.queue_free()
+			
+			var blk_sprite = block.get_child(0)
+			
+			if blk_sprite != null:
+				blk_sprite.texture = EMPTY
+				blk_sprite.z_index = -1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,40 +58,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if HEALTH <= 0 && self.get_child_count() > 1:
-		var collider = self.get_child(1)
-		var coord = self.name.split("Dirt")[1].split("-")
-		
-		# Find destroy all blocks in a circle
-		const neighbors = [
-			[-2, -1], [-2, 0], [-2, 1],
-			[-1, -2], [-1, -1], [-1, 0], [-1, 1], [-1, 2],
-			[0, -2], [0, -1], [0, 0], [0, 1], [0, 2],
-			[1, -2], [1, -1], [1, 0], [1, 1], [1, 2],
-			[2, -1], [2, 0], [2, 1]
-		];
-		
-		for n in neighbors:
-			var x = int(coord[0]) + n[0]
-			var y = int(coord[1]) + n[1]
-			var block = get_node("/root/Node2D/SubViewportContainer/SubViewport/Dirt" + str(x) + "-" + str(y))
-			var blk_collide = block.get_child(1)
-		
-			if blk_collide != null:
-				blk_collide.queue_free()
-			
-			var sprite = block.get_child(0)
-			
-			if sprite != null:
-				sprite.texture = EMPTY
-				sprite.z_index = -1
-		
-		if collider != null:
-			collider.queue_free()
-		
-		var sprite = self.get_child(0)
-		
-		if sprite != null:
-			sprite.texture = EMPTY
-			sprite.z_index = -1
-		
+		explode()
 	pass
